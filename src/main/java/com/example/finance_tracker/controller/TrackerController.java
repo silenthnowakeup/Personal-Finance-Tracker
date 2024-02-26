ca3ef06d847166200b27cca08f5b25af4284f397
silenth <stas.chernyakflexxx@gmail.com>
2024-02-26 09:56:38 +0300

    Add Request

--- src/main/java/com/example/finance_tracker/controller/TrackerController.java
+++ src/main/java/com/example/finance_tracker/controller/TrackerController.java
@@ -1,10 +1,12 @@
 package com.example.finance_tracker.controller;
 import com.example.finance_tracker.service.TrackerService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TrackerController {
+public class TrackerController
+{
     private final TrackerService trackerService;
 
     public TrackerController(TrackerService trackerService)
@@ -12,8 +14,8 @@ public class TrackerController {
         this.trackerService = trackerService;
     }
 
-    @GetMapping("/1")
-    public String tracker() { return trackerService.finance();}
-
-
+    @GetMapping("/users/1")
+    public String tracker(@RequestParam("method") String method) {
+        return trackerService.finance(method);
+    }
 }
--- src/main/java/com/example/finance_tracker/service/TrackerService.java
+++ src/main/java/com/example/finance_tracker/service/TrackerService.java
@@ -1,11 +1,16 @@
 package com.example.finance_tracker.service;
-
 import org.springframework.stereotype.Service;
 
 @Service
-public class TrackerService {
-    public String finance()
+public class TrackerService
+{
+    public String finance(String method)
     {
-        return "123.000,23$";
+        if(method.equals("summary_operations")) {
+            return "123.000,23$";
+        }
+        else {
+            return "Not found";
+        }
     }
 }
